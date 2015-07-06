ed9877b0d654688125d6de279ec818a2e30786b6
Matteo Figus <matteofigus@gmail.com>
2015-07-06 15:44:47 +0100

    Added onError function on nullLogger

--- test/null-logger.js
+++ test/null-logger.js
@@ -6,7 +6,8 @@ describe('The NullLogger', function(){
   it('should implement proper functions that do nothing', function(){
     var logger = LogstashRedis.createNullLogger();
 
+    expect(typeof(logger.close)).to.equal('function');
     expect(typeof(logger.log)).to.equal('function');
-    expect(typeof(logger.close)).to.equal('function')
+    expect(typeof(logger.onError)).to.equal('function');
   });
 });
